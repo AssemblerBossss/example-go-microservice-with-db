4bd9430f8e9bae899b64c34353fc841727c6328c
AssemblerBossss <nazarovmisa39@mail.ru>
2026-07-28 00:27:41 +0300

    feat(sqlite): optimize SQLiteClock with direct Unix timestamp query
    - Replace getTimeFromCurrentTimestamp with getUnixSeconds
    - Use strftime('%s','now') for direct Unix timestamp retrieval
    - Remove intermediate time parsing step
    - Improve performance by using database native integer format

--- internal/infra/sqlite_clock/clock.go
+++ internal/infra/sqlite_clock/clock.go
@@ -21,9 +21,18 @@ func (c *SQLiteClock) getTimeFromCurrentTimestamp() time.Time {
 	return t
 }
 
+func (c *SQLiteClock) getUnixSeconds() int64 {
+	var unixSeconds int64
+	if err := c.DB.QueryRow(`SELECT CAST(strftime('%s','now' ) AS INTEGER);`).Scan(&unixSeconds); err != nil {
+		panic(err)
+	}
+	return unixSeconds
+}
+
 func (c *SQLiteClock) NowUnix() int64 {
-	t := c.getTimeFromCurrentTimestamp()
-	return t.UTC().Unix()
+	//t := c.getTimeFromCurrentTimestamp()
+	//return t.UTC().Unix()
+	return c.getUnixSeconds()
 }
 
 func NewSQLiteClock(db *sql.DB) *SQLiteClock {
